0d003cf348d76b97fba554db995bb78093e25374
RomanBonaparte <roman.cano04@gmail.com>
2025-01-19 17:48:30 +0100

    projet-final

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+public class Main {
+    private static GestionJeu gp;
+
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        Application app = Application.getInstance();
+        app.launch();
+    }
+}
